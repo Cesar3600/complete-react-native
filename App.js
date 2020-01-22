29bee26d4425fb6032a9e206d39d23c19f236e03
cesar <cesar.contreras.medina.3600@gmail.com>
2020-01-21 19:06:33 -0500

    instalado paquetes y probado con expo

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const navigator = createStackNavigator(
   {
     initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: 'App'
+      title: 'este es el titulo de mi aplicacion:'
     }
   }
 );
